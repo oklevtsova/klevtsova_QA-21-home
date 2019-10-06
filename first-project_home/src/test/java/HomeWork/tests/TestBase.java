ac2e859e16ea46d162c263dd7d3b45b934fa6a0f
oklevtsova <54117576+oklevtsova@users.noreply.github.com>
2019-10-06 21:14:40 +0300

    j

--- first-project_home/src/test/java/HomeWork/tests/TestBase.java
+++ first-project_home/src/test/java/HomeWork/tests/TestBase.java
@@ -2,19 +2,21 @@ package HomeWork.tests;
 
 import HomeWork.fw.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
 
 public class TestBase {
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
 
-    @BeforeSuite
+    @BeforeClass
     public  void setUp(){
         app.init();
     }
 
-    @AfterSuite
+    @AfterClass
     public void tearDown() throws InterruptedException {
         app.stop();
     }
