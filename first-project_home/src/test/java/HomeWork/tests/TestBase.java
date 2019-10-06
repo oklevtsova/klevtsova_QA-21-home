ca60ff077a989e64a372df60c376bd09825a64ba
oklevtsova <54117576+oklevtsova@users.noreply.github.com>
2019-10-06 21:47:10 +0300

    a

--- first-project_home/src/test/java/HomeWork/tests/TestBase.java
+++ first-project_home/src/test/java/HomeWork/tests/TestBase.java
@@ -9,7 +9,7 @@ import org.testng.annotations.BeforeSuite;
 
 
 public class TestBase {
-    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeClass
     public  void setUp(){
